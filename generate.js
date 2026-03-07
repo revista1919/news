11b178eea960e7a3a5c7c1cad39816f433041533
revista1919 <revistaestudiantespentauc@gmail.com>
2026-03-07 18:27:38 -0300

    hola

--- generate.js
+++ generate.js
@@ -1025,7 +1025,7 @@ function generateNewsHtmlTemplate({
         </svg>
         ${t.backToNews}
       </a>
-      <a href="/">
+      <a href="${isSpanish ? '/es/' : '/en/'}">
         ${t.backToHome}
         <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
           <path d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
